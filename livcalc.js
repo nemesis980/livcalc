5a3e2e4bafb23268f189cb5c4e27cf5036aed25f
Viktor <viktorsabo@gmail.com>
2018-09-17 18:34:13 +0200

    init 1

--- livcalc.js
+++ livcalc.js
@@ -1,1 +1,5 @@
-console.log(''hello world);
+var restify = require('restify');
+var plotly = require('plotly');
+var react = require('react');
+var angular = require('angular');
+console.log('hello world');
