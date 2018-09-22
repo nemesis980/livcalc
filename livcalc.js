285d38901220a03303e971ea45c4e87e7ae167a7
Viktor Sabo <viktorsabo@gmail.com>
2018-09-22 12:12:32 +0200

    model i varijale bez funkcija...

--- livcalc.js
+++ livcalc.js
@@ -3,6 +3,33 @@ var plotly = require('plotly')
 var react = require('react')
 //var angular = require("angular");
 var cli = require("@angular/cli")
+var mongodb = require("mongodb")
 
 
 console.log('hello livcalc');
+
+		var proizvod;
+		var pitchElement;
+		var perks;
+		var modelPrice;
+		var model;
+		var NDA;
+		var legal;
+
+class cBackerNode {
+	constructor (){
+		this.proizvod;
+		this.pitchElement;
+		this.perks;
+		this.modelPrice;
+		this.model = cModel;
+		this.NDA;
+		this.legal;
+	}
+};
+class cModel {
+	constructor(){
+		var backerModel;
+		var designModel;
+	}
+}
